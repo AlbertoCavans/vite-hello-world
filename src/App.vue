c993358143c30fe9bc405f16958f8598707d9c31
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-12 20:35:40 +0100

    Feat App.vue - basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      title: "Titolo finto, perché il titolo vero sarà nel componente AppTitle",
+    };
+  },
 
-<template></template>
+  methods: {},
+};
+</script>
 
-<style></style>
+<template>
+  <h6 class="font-style">{{ title }}</h6>
+</template>
+
+<style>
+.font-style {
+  font-style: italic;
+}
+</style>
