dcad1829dd4b092e0509fda13356013ff254c9e0
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-12 20:54:44 +0100

    Feat AppTitle.vue - title

---
+++ components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <h1 id="main-title">La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+#main-title {
+  color: crimson;
+}
+</style>
